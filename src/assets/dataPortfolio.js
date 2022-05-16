78f295a546dd2ad94c3456f003ded4b566bb1b5c
Gaël-Freddy GAGOUDE <gael.gagoude@gmail.com>
2022-05-17 01:51:54 +0200

    commit next

--- src/assets/dataPortfolio.js
+++ src/assets/dataPortfolio.js
@@ -1,57 +1,76 @@
 
 export const portfolio = [
     {
-        id: 1,
+        id: 'bootcamp2',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652442847/Portfolio/Portfolio-weather-app_oomebi.png',
+        title: "Weather App",
+        tech: "Bootstrap, NodeJS / Express, Template EJS, Weather API, Leaflet API. ",
+        gitHub: "https://github.com/GAGOUDE/projet-weather-app",
+        demo: "https://github.com/GAGOUDE/projet-weather-app",
+        category: ["all", "application web"]
+    },
+    {
+        id: 'bootcamp1',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652354354/Portfolio/Portfolio-bike-shop_iqcare.png',
         title: "Bike Shop",
-        tech: "Bootstrap, NodeJS / Express, Template EJS, API Stripe",
+        tech: "Bootstrap, NodeJS / Express, Template EJS, API Stripe.",
         gitHub: "https://github.com/GAGOUDE/projet-bike-shop",
         demo: "https://projet-bike-shop.herokuapp.com",
         category: ["all", "application web"]
     },
     {
-        id: 2,
-        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652442847/Portfolio/Portfolio-weather-app_oomebi.png',
-        title: "Weather App",
-        tech: "Bootstrap, NodeJS / Express, Template EJS, Weather API, Leaflet API, ",
-        gitHub: "https://github.com/GAGOUDE/projet-weather-app",
-        demo: "https://github.com",
-        category: ["all", "application web"]
+        id: 'landingManagePage',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio3-ManageLandingPage_uq9vli.png',
+        title: "Responsive Manage Landing Page",
+        tech: "HTML, CSS, JavaScript.",
+        gitHub: "https://github.com/GAGOUDE/Responsive-ManageLandingPage-ChallengeFrontendMentor",
+        demo: "https://landingmanagepage.netlify.app/",
+        category: ["all", "others"]
     },
     {
-        id: 3,
-        image: 'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        title: "Titre de portfolio 3",
+        id: 'productsFilter',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652743962/Portfolio/portfolio-product-filter_q3gb0v.png',
+        title: "Products Filter",
+        tech: "HTML, CSS, jQuery.",
+        gitHub: "https://github.com/GAGOUDE/products-filter",
+        demo: "https://gagoude.github.io/products-filter/",
+        category: ["all", "others"]
+    },
+    {
+        id: 'landingInsurePage',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio2-InsureLandingPage_nxehsv.png',
+        title: "Responsive Insure Landing Page",
+        tech: "HTML, SCSS.",
+        gitHub: "https://github.com/GAGOUDE/Responsive-InsureLandingPage-ChallengeFrontendMentor",
+        demo: "https://landinginsurepage.netlify.app/",
+        category: ["all", "others"]
+    },
+    {
+        id: 'gallerySlider',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652743962/Portfolio/portfolio-slider_idt8qf.png',
+        title: "Gallery slider",
+        tech: "HTML, CSS, Bootstrap.",
+        gitHub: "https://github.com/GAGOUDE/gallery-slider",
+        demo: "https://gagoude.github.io/gallery-slider/",
+        category: ["all", "others"]
+    },
+    {
+        id: 'landingBookmarkPage',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio4-BookmarkLandingPage_wvz6uy.png',
+        title: "Responsive Bookmark Landing Page",
+        tech: "HTML, CSS, JavaScript.",
+        gitHub: "https://github.com/GAGOUDE/Responsive-BookmarkLandingPage-ChallengeFrontEndMentor",
+        demo: "https://landingbookmarkpage.netlify.app/",
+        category: ["all", "others"]
+    },
+    {
+        id: 'landingRoomPage',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio1-RoomHomepage_zwoxde.jpg',
+        title: "Responsive Room Home Page",
         tech: "HTML, CSS, JavaScript",
-        gitHub: "https://github.com",
-        demo: "https://github.com",
-        category: ["all", "application web"]
+        gitHub: "https://github.com/GAGOUDE/Responsive-RoomHomepage-ChallengeFrontendMentor",
+        demo: "https://landingroompage.netlify.app/",
+        category: ["all", "others"]
     },
-    {
-        id: 4,
-        image: 'https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        title: "Titre de portfolio 4",
-        tech: "HTML, CSS, JavaScript, React native, expo",
-        gitHub: "https://github.com",
-        demo: "https://github.com",
-        category: ["all", "application mobile"]
-    },
-    {
-        id: 5,
-        image: 'https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        title: "Titre de portfolio 5",
-        tech: "HTML, CSS, JavaScript, React native, expo",
-        gitHub: "https://github.com",
-        demo: "https://github.com",
-        category: ["all", "application mobile"]
-    },
-    {
-        id: 6,
-        image: 'https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-        title: "Titre de portfolio 6",
-        tech: "HTML, CSS, JavaScript, React native, expo",
-        gitHub: "https://github.com",
-        demo: "https://github.com",
-        category: ["all", "application mobile"]
-    }
+    
 ]
