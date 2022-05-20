52d70c0fffe0f111006e872f1d100c3960fdd9bc
Gaël-Freddy GAGOUDE <gael.gagoude@gmail.com>
2022-05-20 22:34:42 +0200

    actualisation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -28,7 +28,7 @@ function Portfolio() {
     <>
       <section id="portfolio">
         <h5>My Recent Work</h5>
-        <h2>Portfolio <span style={{color: 'red'}}>(Actualisation des projet en cours)</span></h2>
+        <h2>Portfolio <span style={{color: 'red'}}>(Actualisation des PROJETS en cours)</span></h2>
 
         {/* Portfolio Catégories */}
         <div className="portfolio__labels">
