44324209f644741f9445fb5e00273bca8dbaeb58
Gaël-Freddy GAGOUDE <gael.gagoude@gmail.com>
2022-08-04 14:51:10 +0200

    mise à jour

--- src/assets/dataPortfolio.js
+++ src/assets/dataPortfolio.js
@@ -10,15 +10,27 @@ export const portfolio = [
     //     category: ["all", "application web"]
     // },
     {
+        // Food Recipe App
         id: 'persoFoodAppWebsite1',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1653472073/Portfolio/portfolio-food-recipe-app_zaptha.png',
         title: "Food Recipe App",
-        tech: "React JS, Material-UI, Recipe Search API.",
+        tech: "HTML, CSS, React JS, Material-UI, Recipe Search API.",
         gitHub: "https://github.com/GAGOUDE/food-recipe-app",
         demo: "https://food-recipe-website-app.netlify.app",
         category: ["all", "application web"]
     },
     {
+        // Todo App
+        id: 'persoTodoAppWebsite1',
+        image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1659616484/Portfolio/portfolio-todo-app_v7jzfy.png',
+        title: "Food Recipe App",
+        tech: "HTML, CSS, JavaScript, React JS",
+        gitHub: "https://github.com/GAGOUDE/Todo-App",
+        demo: "https://portfolio-todo-app-2022.netlify.app/",
+        category: ["all", "application web"]
+    },
+    {
+        // Générateur de blagues
         id: 'persoBlaguesAleatoires',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1657115130/Portfolio/portfolio-blagues-aleatoires-app_updhw8.png',
         title: "Générateur des blagues aléatoires",
@@ -28,15 +40,17 @@ export const portfolio = [
         category: ["all", "application web"]
     },
     {
+        // Bike Shop
         id: 'bootcamp1',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652354354/Portfolio/Portfolio-bike-shop_iqcare.png',
         title: "Bike Shop",
-        tech: "Bootstrap, Node JS / Express JS, Template EJS, API Stripe.",
+        tech: "HTML, CSS, Bootstrap, Node JS / Express JS, Template EJS, API Stripe.",
         gitHub: "https://github.com/GAGOUDE/projet-bike-shop",
         demo: "https://projet-bike-shop.herokuapp.com",
         category: ["all", "application web"]
     },
     {
+        // Manage Landing Page
         id: 'landingManagePage',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio3-ManageLandingPage_uq9vli.png',
         title: "Responsive Manage Landing Page",
@@ -46,6 +60,7 @@ export const portfolio = [
         category: ["all", "landing-page"]
     },
     {
+        // Products filter
         id: 'productsFilter',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652743962/Portfolio/portfolio-product-filter_q3gb0v.png',
         title: "Products Filter",
@@ -55,6 +70,7 @@ export const portfolio = [
         category: ["all", "others"]
     },
     {
+        // Insecure Landing Page
         id: 'landingInsurePage',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio2-InsureLandingPage_nxehsv.png',
         title: "Responsive Insure Landing Page",
@@ -64,6 +80,7 @@ export const portfolio = [
         category: ["all", "landing-page"]
     },
     {
+        // Gallery slider
         id: 'gallerySlider',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652743962/Portfolio/portfolio-slider_idt8qf.png',
         title: "Gallery slider",
@@ -73,6 +90,7 @@ export const portfolio = [
         category: ["all", "others"]
     },
     {
+        // Bookmark landing page
         id: 'landingBookmarkPage',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio4-BookmarkLandingPage_wvz6uy.png',
         title: "Responsive Bookmark Landing Page",
@@ -82,6 +100,7 @@ export const portfolio = [
         category: ["all", "landing-page"]
     },
     {
+        // room landing page
         id: 'landingRoomPage',
         image: 'https://res.cloudinary.com/dqxk8fn6r/image/upload/v1652738855/Portfolio/Portfolio1-RoomHomepage_zwoxde.jpg',
         title: "Responsive Room Home Page",
